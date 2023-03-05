ad90940851c14b0736f183090ef994a089ad212d
Ankit <ankitkumar151615@gmail.com>
2023-03-06 00:50:19 +0530

    Added question

--- js/question.js
+++ js/question.js
@@ -3,58 +3,113 @@
 const question = [
     {
         num: 1,
-        question: "What does HTML stand for?",
-        answer: "Hyper Text Markup Language",
+        question: "The Battle of Plassey was fought in",
+        answer: "1757",
         option:[
-            "Hyper Text Preprocessor",
-            "Hyper Text Markup Language",
-            "Hyper Text Multi Langauge",
-            "Hyper Tool Markup Language"
+            "1757",
+            "1782",
+            "1748",
+            "1764"
         ]
     },
 
     {
         num: 2,
-        question: "HTML files are saved by default with the extension?",
-        answer: ".html",
+        question: "Under Akbar, the Mir Bakshi was required to look after",
+        answer: "military affairs",
         option:[
-            ".h",
-            ".ht",
-            ".html",
-            "None of the above"
+            "military affairs",
+            "the state treasury",
+            "the royal household",
+            "the land revenue system"
         ]
     },
 
     {
         num: 3,
-        question: "Which attribute is used to provide a unique name to an HTML element?",
-        answer: "id",
+        question: "Tripitakas are sacred books of",
+        answer: "Buddhists",
         option:[
-            "id",
-            "class",
-            "name",
+            "Hindus",
+            "Buddhists",
+            "Jains",
             "None of the above"
         ]
     },
     {
         num: 4,
-        question: "What tag is used to render an image on a webpage?",
-        answer: "img",
+        question: "Grand Central Terminal, Park Avenue, New York is the world's",
+        answer: "largest railway station",
         option:[
-            "img",
-            "src",
-            "image",
+            "highest railway station",
+            "longest railway station",
+            "largest railway station",
             "None of the above"
         ]
     },
     {
         num: 5,
-        question: "Which property is used to set border colors in HTML?",
-        answer: "Both A and B",
+        question: "For which of the following disciplines is Nobel Prize awarded?",
+        answer: "All of the above",
         option:[
-            "border",
-            "border-color",
-            "Both A and B",
+            "Physics and Chemistry",
+            "Physiology or Medicine",
+            "Literature, Peace and Economics",
+            "All of the above"
+        ]
+    },
+    {
+        num: 6,
+        question: "The members of the Rajya Sabha are elected by",
+        answer: "elected members of the legislative assembly",
+        option:[
+            "the people",
+            "Lok Sabha",
+            "elected members of the legislative assembly",
+            "elected members of the legislative council"
+        ]
+    },
+    {
+        num: 7,
+        question: "The power to decide an election petition is vested in the",
+        answer: "High courts",
+        option:[
+            "Parliament",
+            "Supreme Court",
+            "High courts",
+            "Election Commission"
+        ]
+    },
+    {
+        num: 8,
+        question: "For which of the following disciplines is Nobel Prize awarded?",
+        answer: "All of the above",
+        option:[
+            "Physics and Chemistry",
+            "Physiology or Medicine",
+            "Literature, Peace and Economics",
+            "All of the above"
+        ]
+    },
+    {
+        num: 9,
+        question: "The system of competitive examination for civil service was accepted in principle in the year",
+        answer: "1853",
+        option:[
+            "1833",
+            "1853",
+            "1858",
+            "1882"
+        ]
+    },
+    {
+        num: 10,
+        question: "FFC stands for",
+        answer: "Film Finance Corporation",
+        option:[
+            "Foreign Finance Corporation",
+            "Film Finance Corporation",
+            "Federation of Football Council",
             "None of the above"
         ]
     },
